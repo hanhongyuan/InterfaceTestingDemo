b058bcb8d91d42639cfd07d1f136e5044a36c3ce
vivian <448000969@qq.com>
2016-06-26 10:57:34 +0800

    establish interface projects

---
+++ interfaceTesting/src/test/java/utils/HTTPReqGenTest.java
@@ -0,0 +1,16 @@
+package utils;
+
+import org.testng.ITest;
+
+import com.jayway.restassured.response.Response;
+
+public class HTTPReqGenTest implements ITest {
+	
+	private Response response;
+
+	public String getTestName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
