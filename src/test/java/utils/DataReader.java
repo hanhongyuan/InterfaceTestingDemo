0e5cc2d54dd672e6795b44071b5899ae5e5f7db9
shirui <shirui@ucredit.com>
2016-06-27 18:41:44 +0800

    update

--- src/test/java/utils/DataReader.java
+++ src/test/java/utils/DataReader.java
@@ -1,5 +1,136 @@
 package utils;
 
-public class DataReader {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.logging.Logger;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
 
+public class DataReader {
+	// protected static final Logger logger =
+	// LoggerFactory.getLogger(DataReader.class);
+
+	private HashMap<String, RecordHandler> map = new HashMap<String, RecordHandler>();
+	private Boolean booleanColumnName = false;
+	private Boolean booleanRowKey = false;
+	private List<String> headers = new ArrayList<String>();
+	private Integer size = 0;
+
+	public DataReader() {
+	}
+
+	/**
+	 * Primary constructor. Uses Apache POI XSSF to pull data from given excel
+	 * workbook sheet. Data is stored in a structure depending on the options
+	 * from other parameters.
+	 * 
+	 * @param sheet
+	 *            Given excel sheet.
+	 * @param has_headers
+	 *            Boolean used to specify if the data has a header or not. The
+	 *            headers will be used as field keys.
+	 * @param has_key_column
+	 *            Boolean used to specify if the data has a column that should
+	 *            be used for record keys.
+	 * @param key_column
+	 *            Integer used to specify the key column for record keys.
+	 */
+	public DataReader(XSSFSheet sheet, Boolean has_headers, Boolean has_key_column, Integer key_column) {
+
+		XSSFRow myRow = null;
+		HashMap<String, String> myList;
+		size = 0;
+
+		this.booleanColumnName = has_headers;
+		this.booleanRowKey = has_key_column;
+
+		// 如果有表头,获取当前的表头信息,得到表头的list,并且设置Size=1
+		try {
+			if (booleanColumnName) {
+				myRow = sheet.getRow(0);
+				for (Cell cell : myRow) {
+					headers.add(cell.getStringCellValue());
+				}
+				size = 1;
+			}
+
+			// 每一行代表一个Record，对于每一个Record可以获取长度，读写或者是更新
+			// size作为指针往下读数据，size++一直到sheet.getRow的值为空，停止按行读入数据
+			for (; (myRow = sheet.getRow(size)) != null; size++) {
+				myList = new HashMap<String, String>();
+				// 判断是否有表头，然后读取内容；如果当前的列内容小于表头的列，则直接保持为空
+				if (booleanColumnName) {
+					for (int col = 0; col < headers.size(); col++) {
+						if (col < myRow.getLastCellNum()) {
+							myList.put(headers.get(col), getSheetCellValue(myRow.getCell(col))); // myRow.getCell(col).getStringCellValue());
+						} else {
+							myList.put(headers.get(col), "");
+						}
+					}
+				} else {
+					for (int col = 0; col < myRow.getLastCellNum(); col++) {
+						myList.put(Integer.toString(col), getSheetCellValue(myRow.getCell(col)));
+					}
+				}
+
+				// booleanRowKey：是否存在此列内容的Key，如果有，指出key_column;如果没有，直接按index的顺序存储record
+				if (booleanRowKey) {
+					if (myList.size() == 2 && key_column == 0) {
+						map.put(getSheetCellValue(myRow.getCell(key_column)), new RecordHandler(myList.get(1)));
+					} else if (myList.size() == 2 && key_column == 1) {
+						map.put(getSheetCellValue(myRow.getCell(key_column)), new RecordHandler(myList.get(0)));
+					} else {
+						map.put(getSheetCellValue(myRow.getCell(key_column)), new RecordHandler(myList));
+					}
+				} else {
+					map.put(Integer.toString(size), new RecordHandler(myList));
+				}
+			}
+			
+		} catch (Exception e) {
+			// logger.error("Exception while loading data from Excel
+			// sheet:"+e.getMessage());
+		}
+	}
+
+	/**
+	 * 将指定cell的内容转化为String
+	 */
+	private String getSheetCellValue(XSSFCell cell) {
+
+		String value = "";
+		try {
+			cell.setCellType(Cell.CELL_TYPE_STRING);
+			value = cell.getStringCellValue();
+		} catch (NullPointerException npe) {
+			return "";
+		}
+		return value;
+	}
+
+	/**
+	 * 获取代表整个sheet页面的HashMap
+	 */
+	public HashMap<String, RecordHandler> get_map() {
+
+		return map;
+	}
+
+	/**
+	 * Gets an entire record.
+	 * 
+	 * @param record
+	 *            String key value for record to be returned.
+	 * @return HashMap of key-value pairs representing the specified record.
+	 */
+	public RecordHandler get_record(String record) {
+		RecordHandler result = new RecordHandler();
+		if (map.containsKey(record)) {
+			result = map.get(record);
+		}
+		return result;
+	}
 }
--- src/test/java/utils/RecordHandler.java
+++ src/test/java/utils/RecordHandler.java
@@ -1,5 +1,178 @@
 package utils;
 
-public class RecordHandler {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
+/**
+ * Record处理器分三种，分别是Value/Map/Index_List
+ * @author admin
+ *
+ */
+public class RecordHandler {
+	private enum RecordType {
+		VALUE, NAMED_MAP, INDEXED_LIST
+	}
+
+	private String single_value = "";
+	private HashMap<String, String> named_value_map = new HashMap<String, String>();
+	private List<String> indexed_value_list = new ArrayList<String>();
+	private RecordType recordType;
+	
+	
+    //----------------------不同类型的构造函数-------------------------------------------------------
+	public RecordHandler() {
+		this("");
+	}
+
+	public RecordHandler(String value) {
+		this.recordType = RecordType.VALUE;
+		this.single_value = value;
+	}
+
+	public RecordHandler(HashMap<String, String> map) {
+		this.recordType = RecordType.NAMED_MAP;
+		this.named_value_map = map;
+	}
+
+	public RecordHandler(List<String> list) {
+
+		this.recordType = RecordType.INDEXED_LIST;
+		this.indexed_value_list = list;
+
+	}
+
+	public HashMap<String, String> get_map() {
+		return named_value_map;
+	}
+
+	//--------------------获取不同Record的长度-----------------------------------------------------
+	public int size() {
+		int result = 0;
+
+		if (recordType.equals(RecordType.VALUE)) {
+			result = 1;
+		} else if (recordType.equals(RecordType.NAMED_MAP)) {
+			result = named_value_map.size();
+		} else if (recordType.equals(RecordType.INDEXED_LIST)) {
+			result = indexed_value_list.size();
+		}
+
+		return result;
+	}
+
+	//----------------------获取不同类型的Record的内容------------------------------------------
+	//如果是Value，直接获取值；如果是Map，通过Key获取；如果是List，通过Index获取
+	public String get() {
+		String result = "";
+		if (recordType.equals(RecordType.VALUE))
+			result = single_value;
+		else {
+			System.out.println("Called get() on wrong type:" + recordType.toString());
+		}
+		return result;
+	}
+
+	public String get(String key) {
+		String result = "";
+
+		if (recordType.equals(RecordType.NAMED_MAP))
+			result = named_value_map.get(key);
+
+		return result;
+	}
+
+	public String get(Integer index) {
+		String result = "";
+
+		if (recordType.equals(RecordType.INDEXED_LIST))
+			result = indexed_value_list.get(index);
+
+		return result;
+	}
+
+	public Boolean set(String value) {
+		Boolean result = false;
+
+		if (recordType.equals(RecordType.VALUE)) {
+			this.single_value = value;
+			result = true;
+		} else if (recordType.equals(RecordType.INDEXED_LIST)) {
+			this.indexed_value_list.add(value);
+			result = true;
+		}
+
+		return result;
+	}
+
+	//--------------------------设置Record的值--------------------------------------
+	//如果是value直接设置，list可以添加或者是改变指定index的值，map添加键值对
+	public Boolean set(String key, String value) {
+		Boolean result = false;
+
+		if (recordType.equals(RecordType.NAMED_MAP)) {
+			this.named_value_map.put(key, value);
+			result = true;
+		}
+
+		return result;
+	}
+
+	public Boolean set(Integer index, String value) {
+		Boolean result = false;
+
+		if (recordType.equals(RecordType.INDEXED_LIST)) {
+			if (this.indexed_value_list.size() > index)
+				this.indexed_value_list.set(index, value);
+
+			result = true;
+		}
+
+		return result;
+	}
+
+	//判断是否有值
+	public Boolean has(String value) {
+		Boolean result = false;
+
+		if (recordType.equals(RecordType.VALUE) && this.single_value.equals(value)) {
+			result = true;
+		} else if (recordType.equals(RecordType.NAMED_MAP) && this.named_value_map.containsKey(value)) {
+			result = true;
+		} else if (recordType.equals(RecordType.INDEXED_LIST) && this.indexed_value_list.contains(value)) {
+			result = true;
+		}
+
+		return result;
+	}
+
+	//判断是否删除值
+	public Boolean remove(String value) {
+		Boolean result = false;
+
+		if (recordType.equals(RecordType.VALUE) && this.single_value.equals(value)) {
+			this.single_value = "";
+			result = true;
+		}
+		if (recordType.equals(RecordType.NAMED_MAP) && this.named_value_map.containsKey(value)) {
+			this.named_value_map.remove(value);
+			result = true;
+		} else if (recordType.equals(RecordType.INDEXED_LIST) && this.indexed_value_list.contains(value)) {
+			this.indexed_value_list.remove(value);
+			result = true;
+		}
+
+		return result;
+	}
+
+	public Boolean remove(Integer index) {
+		Boolean result = false;
+
+		if (recordType.equals(RecordType.INDEXED_LIST) && this.indexed_value_list.contains(index)) {
+			this.indexed_value_list.remove(index);
+			result = true;
+		}
+
+		return result;
+	}
 }
